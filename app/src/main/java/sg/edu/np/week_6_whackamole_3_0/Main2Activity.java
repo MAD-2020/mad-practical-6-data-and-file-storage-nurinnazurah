dd28d52a0c9e13fb47ed38fb778854607265ba86
Nazurah <s10189240@connect.np.edu.sg>
2020-08-12 23:58:55 +0800

    update

--- app/src/main/java/sg/edu/np/week_6_whackamole_3_0/Main2Activity.java
+++ app/src/main/java/sg/edu/np/week_6_whackamole_3_0/Main2Activity.java
@@ -3,46 +3,63 @@ package sg.edu.np.week_6_whackamole_3_0;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 import java.util.ArrayList;
 
 public class Main2Activity extends AppCompatActivity {
-    /* Hint:
-        1. This is the create new user page for user to log in
-        2. The user can enter - Username and Password
-        3. The user create is checked against the database for existence of the user and prompts
-           accordingly via Toastbox if user already exists.
-        4. For the purpose the practical, successful creation of new account will send the user
-           back to the login page and display the "User account created successfully".
-           the page remains if the user already exists and "User already exist" toastbox message will appear.
-        5. There is an option to cancel. This loads the login user page.
-     */
-
 
     private static final String FILENAME = "Main2Activity.java";
     private static final String TAG = "Whack-A-Mole3.0!";
+
+    MyDBHandler dbHandler = new MyDBHandler(this,null,null,1);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
-        /* Hint:
-            This prepares the create and cancel account buttons and interacts with the database to determine
-            if the new user created exists already or is new.
-            If it exists, information is displayed to notify the user.
-            If it does not exist, the user is created in the DB with default data "0" for all levels
-            and the login page is loaded.
+        Button create = findViewById(R.id.buttonCreate);
+        create.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                EditText etCreateUsername = findViewById(R.id.editText_CreateUsername);
+                EditText etCreatePassword = findViewById(R.id.editText_CreatePassword);
 
-            Log.v(TAG, FILENAME + ": New user created successfully!");
-            Log.v(TAG, FILENAME + ": User already exist during new user creation!");
+                UserData userData = dbHandler.findUser(etCreateUsername.getText().toString());
+                if(userData == null)
+                {
+                    UserData dbUserData = new UserData();
+                    dbUserData.setMyUserName(etCreateUsername.getText().toString());
+                    dbUserData.setMyPassword(etCreatePassword.getText().toString());
+                    dbHandler.addUser(dbUserData);
+                    Toast.makeText(Main2Activity.this, "New User Created!", Toast.LENGTH_SHORT).show();
+                    Log.v(TAG, FILENAME + "Created: " + etCreateUsername.getText().toString() );
+                    Intent intent = new Intent(Main2Activity.this, MainActivity.class);
+                    startActivity(intent);
+                }
+                else
+                {
+                    Log.v(TAG, FILENAME + ": User already exist during new user creation!");
+                    Toast.makeText(Main2Activity.this, "User already exist. \nPlease try again", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
-         */
+        Button cancel = findViewById(R.id.buttonCancel);
+        cancel.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(Main2Activity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     protected void onStop() {
--- app/src/main/java/sg/edu/np/week_6_whackamole_3_0/MainActivity.java
+++ app/src/main/java/sg/edu/np/week_6_whackamole_3_0/MainActivity.java
@@ -3,6 +3,7 @@ package sg.edu.np.week_6_whackamole_3_0;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MotionEvent;
@@ -24,22 +25,48 @@ public class MainActivity extends AppCompatActivity {
     private static final String FILENAME = "MainActivity.java";
     private static final String TAG = "Whack-A-Mole3.0!";
 
+    MyDBHandler dbHandler = new MyDBHandler(this, null, null, 1);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        /* Hint:
-            This method creates the necessary login inputs and the new user creation ontouch.
-            It also does the checks on button selected.
-            Log.v(TAG, FILENAME + ": Create new user!");
-            Log.v(TAG, FILENAME + ": Logging in with: " + etUsername.getText().toString() + ": " + etPassword.getText().toString());
-            Log.v(TAG, FILENAME + ": Valid User! Logging in");
-            Log.v(TAG, FILENAME + ": Invalid user!");
+        TextView newUser = findViewById(R.id.textView_NewUser);
+        newUser.setOnTouchListener(new View.OnTouchListener(){
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent){
+                Log.v(TAG, FILENAME + ": Create new user!");
 
-        */
+                Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+                startActivity(intent);
 
+                return false;
+            }
+        });
 
+        Button login = findViewById(R.id.buttonLogin);
+        login.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                EditText etUsername = findViewById(R.id.editText_Username);
+                EditText etPassword = findViewById(R.id.editText_Password);
+
+                Log.v(TAG, FILENAME + ": Logging in with: " + etUsername.getText().toString() + ": " + etPassword.getText().toString());
+                if(isValidUser(etUsername.getText().toString(), etPassword.getText().toString())){
+                    Log.v(TAG, FILENAME + ": Valid User! Logging in");
+                    Toast.makeText(MainActivity.this, "User Login Successfully!", Toast.LENGTH_SHORT).show();
+
+                    Intent intent = new Intent(MainActivity.this, Main3Activity.class);
+                    startActivity(intent);
+                }
+                else
+                {
+                    Log.v(TAG, FILENAME + ": Invalid user!");
+                    Toast.makeText(MainActivity.this, "Invalid Username/Password", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
     protected void onStop(){
@@ -48,13 +75,13 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public boolean isValidUser(String userName, String password){
-
-        /* HINT:
-            This method is called to access the database and return a true if user is valid and false if not.
-            Log.v(TAG, FILENAME + ": Running Checks..." + dbData.getMyUserName() + ": " + dbData.getMyPassword() +" <--> "+ userName + " " + password);
-            You may choose to use this or modify to suit your design.
-         */
-
+        UserData dbData = dbHandler.findUser(userName);
+        Log.v(TAG, FILENAME + ": Running Checks..." + dbData.getMyUserName() + ": " + dbData.getMyPassword() +" <--> "+ userName + " " + password);
+        if(dbData.getMyUserName().equals(userName) && dbData.getMyPassword().equals(password))
+        {
+            return true;
+        }
+        return false;
     }
 
 }
